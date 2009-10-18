09710e5045b43b220ad0aab85017c933b30b09fe
tyru <tyru.exe@gmail.com>
2009-10-19 02:18:10 +0900

    separate declarations into dentaku.h

---
+++ dentaku.h
@@ -0,0 +1,82 @@
+#ifndef DENTAKU_H
+#define DENTAKU_H
+
+
+
+#include "common.h"
+
+#include "util.h"
+#include "token.h"
+#include "parser.h"
+
+#include "mylib/stack.h"
+
+
+#include <stdarg.h>
+
+
+
+
+
+
+typedef struct {
+    Stack   *cur_stack;    // easier to access
+    Stack   cur_stack__;
+
+    FILE    *f_in;
+    FILE    *f_out;
+    FILE    *f_err;
+
+    char    *src;
+    int     src_pos;
+    int     src_len;
+} Dentaku;
+
+
+
+
+stack_ret
+dentaku_stack_pop(Dentaku *dentaku, Token *tok);
+
+stack_ret
+dentaku_stack_push(Dentaku *dentaku, Token *tok);
+
+bool
+dentaku_src_eof(Dentaku *dentaku);
+
+bool
+dentaku_calc_expr(Dentaku *dentaku, Token *tok_result, bool *done);
+
+bool
+dentaku_eval_expr(Dentaku *dentaku, bool *done_eval_expr);
+
+Token*
+dentaku_get_token(Dentaku *dentaku, bool *error);
+
+
+
+void
+dentaku_init(Dentaku *dentaku);
+
+void
+dentaku_alloc(Dentaku *dentaku, size_t stack_size);
+
+void
+dentaku_destroy(Dentaku *dentaku);
+
+
+
+bool
+dentaku_read_src(Dentaku *dentaku);
+
+bool
+dentaku_eval_src(Dentaku *dentaku);
+
+void
+dentaku_clear_stack(Dentaku *dentaku);
+
+void
+dentaku_show_result(Dentaku *dentaku);
+
+
+#endif /* DENTAKU_H */
